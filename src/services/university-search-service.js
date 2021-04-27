2a8e7dca89aca52f98e59fa914f3e1e17ea16fbe
AnirudhPolisetty <71300234+AnirudhPolisetty@users.noreply.github.com>
2021-04-27 04:07:00 -0400

    Update university-search-service.js

--- src/services/university-search-service.js
+++ src/services/university-search-service.js
@@ -1,12 +1,11 @@
-const proxyurl = "https://ohuapi.herokuapp.com/universities/";
 const UNIVERSITY_URL = "http://universities.hipolabs.com/search?";
 
 export const findAllUniversitiesByName = (name, country) =>
-    fetch(`${proxyurl}+${UNIVERSITY_URL}name=${name}&country=${country}`)
+    fetch(`${UNIVERSITY_URL}name=${name}&country=${country}`)
         .then(response => response.json())
 
 export const findAllUniversitiesByOnlyName = (name) =>
-    fetch(`${proxyurl}+${UNIVERSITY_URL}name=${name}`)
+    fetch(`${UNIVERSITY_URL}name=${name}`)
         .then(response => response.json())
 
 export default {
